fa6955f7b9603aa1726167ad55482e9489853c92
John Corn <jcornacchioli@gmail.com>
2023-06-03 04:21:10 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers= function (d  = ['Antonia', 'Nuru', 'Amari', 'Mo']) 
+{
+    const firstTwoDrivers = d.slice(0, 2);
+    return (firstTwoDrivers);
+};
+
+
+const returnLastTwoDrivers= function (d  = ['Antonia', 'Nuru', 'Amari', 'Mo']) 
+{
+    const lastTwoDrivers = d.slice(2, d.lenght);
+    return (lastTwoDrivers);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (n)
+{
+    const fare = 1;
+
+    switch(n)
+    {
+        case 2:
+        return function (fare) {
+            return fareDoubler(fare);
+        };
+
+        case 3:
+            return function (fare) {
+                return fareTripler(fare);
+            };
+
+            
+        case 5:
+            return function (fare) {
+                return fareQuintupler(fare);
+            };
+
+        default:
+            return fare * n;
+    }
+}
+
+function fareDoubler (fare)
+{
+    return (fare *2); 
+}
+
+function fareTripler (fare)
+{
+    return (fare *3); 
+}
+
+function fareQuintupler (fare)
+{
+    return (fare * 5); 
+}
+
+function selectDifferentDrivers(d  = ['Antonia', 'Nuru', 'Amari', 'Mo'], returnFunction)
+{
+    return returnFunction (d);
+}
